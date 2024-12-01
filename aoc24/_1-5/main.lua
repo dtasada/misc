3ae145658b994e3b2b5df779b845f98b8507cfbe
Daniel Tasada <daniel.tasada@icloud.com>
2024-12-01 21:30:46 +0100

    aoc24 d1

---
+++ aoc24/_1-5/main.lua
@@ -0,0 +1,47 @@
+local io = require("io")
+
+local function main()
+	local file = io.open("day1.txt", "r")
+
+	if not file then
+		return nil
+	end
+
+	local left = {}
+	local right = {}
+
+	for line in file:lines() do
+		line:gsub("(%d+)   (%d+)", function(a, b)
+			table.insert(left, a)
+			table.insert(right, b)
+		end)
+	end
+
+	table.sort(left)
+	table.sort(right)
+
+	local p1 = 0
+	for i = 1, #left do
+		p1 = p1 + require("math").abs(right[i] - left[i])
+	end
+	print("p1", p1)
+
+	-- Part 2
+	local p2 = 0
+	for i = 1, #left do
+		local count = 0
+		for j = 1, #right do
+			if right[j] == left[i] then
+				count = count + 1
+			end
+		end
+
+		p2 = p2 + count * left[i]
+	end
+
+	print("p2", p2)
+
+	file:close()
+end
+
+main()
